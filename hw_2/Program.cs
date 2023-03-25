876a5b946ca4e51f06124526d9f24f703d1b80c0
Nazik <nazikbaktygulova77@gmail.com>
2023-03-25 12:37:11 +0600

    Написан код для задачи №66 в папке hw_2

---
+++ hw_2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+
+void NumberSummary (int number1, int number2, int sum)
+{
+    if (number1 > number2) 
+    {
+        Console.WriteLine($"Сумма натуральных чисел в промежутке от M до N: {sum}"); 
+        return;
+    }
+    sum = sum + (number1++);
+    NumberSummary(number1, number2, sum);
+}
+
+Console.Write("Введите первое число: ");
+int M = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+NumberSummary(M, N, 0);
+
