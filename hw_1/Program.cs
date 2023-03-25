abd915abd0fb13f01f990a84b35aa67bf9f76eab
Nazik <nazikbaktygulova77@gmail.com>
2023-03-25 11:57:06 +0600

    Написан код для задачи №64 в папке hw_1

---
+++ hw_1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+
+Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+void NumberPrint (int val)
+{
+    if (val < 0) 
+        Console.WriteLine("Введите натуральное число!");
+    if (val == 0) 
+        return;
+        Console.Write("{0,4}", val);
+    
+    NumberPrint (val - 1);
+}
+
+NumberPrint(N);
